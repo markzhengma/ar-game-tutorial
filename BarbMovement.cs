e0d0a3060065889bcd66e870165c5abbb81e997d
Mark Ma <mazheng1022@hotmail.com>
2018-02-12 13:17:33 -0500

    models, mechanics set up

---
+++ BarbMovement.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BarbMovement : MonoBehaviour {
+
+	public GameObject guy;
+	public Transform target;
+
+	private GameObject[] enemies;
+
+	private bool isRunning;
+
+	private bool isDead;
+
+	// Use this for initialization
+	void Start () {
+		// collide = false;
+		isRunning = true;
+		GetComponentInChildren<Animator> ().Play("Run");
+		enemies = GameObject.FindGameObjectsWithTag("Enemy");
+	}
+	
+	// Update is called once per frame
+	void Update(){
+		isDead = guy.GetComponent<GuyMovement>().isDead;
+		if(!isDead){
+			if(isRunning){
+				transform.position = Vector3.MoveTowards(transform.position, guy.transform.position, 0.01f);
+			}
+
+			transform.LookAt(target.transform.position);
+		}
+		else
+		{
+			if(isRunning){
+				GetComponentInChildren<Animator> ().Play("Walk");
+			}
+		}
+		
+	}
+
+	void OnCollisionEnter(Collision other) 
+	{
+		if (other.gameObject.CompareTag ("Guy")) 
+		{
+			GetComponentInChildren<Animator> ().Play("RoundKick");
+			guy.GetComponent<GuyMovement>().Die();
+			StartCoroutine(SetRunning(3.0f));
+		}
+	}
+
+	public IEnumerator SetRunning (float waitTime) {
+		isRunning = false;
+		yield return new WaitForSeconds(waitTime);
+		isRunning = true;
+	}
+}
---
+++ GuyMovement.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class GuyMovement : MonoBehaviour {
+
+	private Rigidbody rb;
+	public GameObject EnemyObject;
+	public Transform enemy;
+	private bool isFiring;
+	public bool isDead;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+		isFiring = false;
+		isDead = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(!isDead){
+			transform.LookAt(enemy.transform.position);
+
+			float x = CrossPlatformInputManager.GetAxis ("Horizontal");
+			float z = CrossPlatformInputManager.GetAxis ("Vertical");
+
+			Vector3 movement = new Vector3 (x, 0, z);
+
+			rb.velocity = movement * 1f;
+
+			if(!isFiring){
+				if (x != 0 && z != 0) {
+					transform.eulerAngles = new Vector3 (transform.eulerAngles.x, Mathf.Atan2(x, z) * Mathf.Rad2Deg, transform.eulerAngles.z);
+				}
+
+				if (x != 0 || z != 0) {
+					GetComponentInChildren<Animator> ().Play("Walk");
+				}
+				else 
+				{
+					GetComponentInChildren<Animator> ().Play("Idle");
+				}
+			}
+		}
+	}
+
+	public void Fire () {
+		if(!isFiring && !isDead){
+			GetComponentInChildren<Animator> ().Play("Shoot");
+			StartCoroutine(EnemyObject.GetComponent<BarbMovement>().SetRunning(1.0f));
+			StartCoroutine(SetFiring());
+		}
+	}
+
+	IEnumerator SetFiring () {
+		isFiring = true;
+		yield return new WaitForSeconds(1.0f);
+		isFiring = false;
+	}
+
+	public void Die () {
+		GetComponentInChildren<Animator> ().Play("Die");
+		isDead = true;
+	}
+}
